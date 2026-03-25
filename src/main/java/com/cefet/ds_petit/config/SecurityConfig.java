7c2bbb7030f8938d6f865618a8f81bd28aa65166
LHSDias <luizhenriquemc15@gmail.com>
2026-03-25 11:13:27 -0300

    WebConfig

--- src/main/java/com/cefet/ds_petit/config/SecurityConfig.java
+++ src/main/java/com/cefet/ds_petit/config/SecurityConfig.java
@@ -122,7 +122,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins("http://localhost:4200", "https://biblioteca-petit.netlify.app/")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
